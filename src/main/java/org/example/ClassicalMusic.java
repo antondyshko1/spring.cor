0cadb22ac51c7017a95f7e19a82dc3d4e95db31f
anton <antondyshko1@gmail.com>
2022-06-24 14:31:09 +0300

    commit

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,6 +1,16 @@
 package org.example;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic(){}// Приватный конструктор, говороит о том, что объекты этого класса нужно создавать с помищью фабричного метода
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+    public void doMyDestroy(){
+        System.out.println("Do my destruction");
+    }
     @Override
     public String getSong(){
         return "Hungarian Rhapsody";
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,12 +7,15 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(  // Класс обращается к applicationContext.xml расположенном в resources, считывает его и помещает все бины которые там описаны в ApplicationContext
                 "applicationContext.xml"
         );
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
         //Music music = context.getBean("musicBean", Music.class);// Получаем бин getBean() из context, в параметр передаем id из xml и класс бин(объект) которого хотим получить. Присваиваем это все переменной music
         //MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        //MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        //musicPlayer.playMusicList();
+        //System.out.println(musicPlayer.getName());
+        //System.out.println(musicPlayer.getVolume());
         context.close(); // Обязательно нужно закрыть applicationContext
     }
 }
